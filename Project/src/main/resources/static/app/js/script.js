f612f32a6e9bfda89a06215e8ad798a7150e803a
htiwari1402 <htiwari1402@gmail.com>
2016-12-04 20:21:05 +0530

    Migration changes

--- Project/src/main/resources/static/app/js/script.js
+++ Project/src/main/resources/static/app/js/script.js
@@ -26,3 +26,55 @@ function login()
 			);
 	
 }
+
+function migrate()
+{
+	var mig_opt = $('#mig_opt').val();
+	var src_domain = $('#src_domain').va();
+	var tgt_domain = $('#tgt_domain').val();
+	var src_repo = $('#src_repo').val();
+	var tgt_repo = $('#tgt_repo').val();
+	var dep_group = $('#dep_group').val();
+	var emailGroup = $('#emailGroup').val();
+}
+
+function addEmail()
+{
+	var textval = $('#emailGroup').val();
+	var currEmail=$('#email').val();
+	textval = textval+','+currEmail;
+	$('#emailGroup').val(textval);
+}
+
+$(document).ready(function(){
+	$.post("getRepo",function(responseText)
+			{
+				$('#src_repo').html(responseText);
+				$('#tgt_repo').html(responseText);
+			});
+	$.post("getDom",function(responseText)
+			{
+				$('#src_domain').html(responseText);
+				$('#tgt_domain').html(responseText);
+			});
+	$.post("get_tkt_no",function(responseText)
+			{
+				$('#tkt_no_show').html(responseText);
+				$('#tkt_no').val(responseText);
+			});
+});
+
+function migrate()
+{
+	var control_param_text = $('#control_param_text').val();
+	var postData = {
+						"control_param_text":control_param_text
+					};
+	$.post("genXML",postData,function(response)
+			{
+				if(parseInt(response) == 1)
+					{
+						$('#mig_form').submit();
+					}
+			});
+}
